1f03d5ae0d1397f3b152c28762e6ca34547329af
Victor Garcia <kkvictin69@gmail.com>
2025-04-17 20:39:03 -0300

    first commit

---
+++ C/Jogo do Número secreto/game.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    printf("*************************************\n"); 
+    printf("*Bem Vindo ao JOGO DO NÚMERO SECRETO*\n");
+    printf("*************************************\n");
+
+    int numeroSecreto = 12;
+
+    int chute;
+
+    printf("Digite seu chute: ");
+    scanf("%d", &chute);
+    printf("Seu chute foi %d", chute);
+
+     
+}
---
+++ C/aula 1/aula1.c
@@ -0,0 +1,15 @@
+#include <stdio.h> // importa a biblioteca para escrita no C/C++
+#include <stdbool.h> // importa a biblioteca para a utilização de booleano em C/C++
+
+
+int main(){ // começa o código
+    printf("**********\n"); //  imprime algo no console
+    printf("*UOU*\n"); //  "\n" quebra linha, TEM QUE SER DENTRO DA STRING
+    printf("**********\n"); //  SEMPRE finalizar uma ação com ";"
+
+    int numeroSecreto = 42; //  variável de números inteiros
+    char nome = "Victor"; //  variável de palavras
+    double pi = 3.14; //  variável de números decimais 
+    bool macOS = true; //  variável booleana (true or false)
+
+}
